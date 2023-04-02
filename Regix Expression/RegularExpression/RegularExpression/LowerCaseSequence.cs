32ff38d531b761d0ccb0e9d09e5ed3fc1ab13dae
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-04-02 14:26:26 +0530

    [Prashant]Add: Program to find sequences of lowercase joind by underscore

---
+++ Regix Expression/RegularExpression/RegularExpression/LowerCaseSequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class LowerCaseSequence
+    {
+        public static void CheckLowerCase()
+        {
+            Console.WriteLine("Enter the name: ");
+            string word = Console.ReadLine();
+            Regex regex = new Regex("^[a-z]+_[a-z]+$");
+            if(regex.IsMatch(word))
+            {
+                Console.WriteLine("String is valid");
+            }
+            else
+            {
+                Console.WriteLine("String is not valid");
+            }
+        }
+    }
+}
--- Regix Expression/RegularExpression/RegularExpression/Program.cs
+++ Regix Expression/RegularExpression/RegularExpression/Program.cs
@@ -7,13 +7,18 @@ namespace RegularExpression
         {
             Console.WriteLine("Welcome to Regular Expression");
             Console.WriteLine("Input options to perform program");
-            Console.WriteLine("1:String Match");
+            Console.WriteLine("1:String Match\n2:Lower case");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     StringMatch.CheckStringMatch();
                     break;
+                case 2:
+                    LowerCaseSequence.CheckLowerCase();
+                    break;
+
+
             }
         }
     }
