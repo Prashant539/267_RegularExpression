3e2716ca1a4449db46bc0a49c85da31439e9e02b
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-04-02 12:13:31 +0530

    [Prashant]Add: Program that matches a string

---
+++ Regix Expression/RegularExpression/RegularExpression/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression");
+            Console.WriteLine("Input options to perform program");
+            Console.WriteLine("1:String Match");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    StringMatch.CheckStringMatch();
+                    break;
+            }
+        }
+    }
+}
---
+++ Regix Expression/RegularExpression/RegularExpression/StringMatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class StringMatch
+    {
+        public static void CheckStringMatch()
+        {
+            string input = "abbb";
+            string pattern = "^[a]{1}[b]{2,3}$";
+            Regex regex = new Regex(pattern);
+            if(regex.IsMatch(input))
+            {
+                Console.WriteLine("String is matches with pattern ");
+            }
+            else
+            {
+                Console.WriteLine("String is not matches with pattern"); 
+            }
+        }
+    }
+}
